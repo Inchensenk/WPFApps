5213b86d085a9c9964deca6f60e3c42d96c518d8
Inchensenk <aab8117@gmail.com>
2022-12-15 14:45:00 +0300

    Получение данных. Отслеживание ошибок
    https://www.youtube.com/watch?v=QIUJAJ7Udqw&list=PL0lO_mIqDDFVI0xwaYbm7h9ewYu5hftfA&index=4

--- WPFApps/MainWindow.xaml.cs
+++ WPFApps/MainWindow.xaml.cs
@@ -24,5 +24,53 @@ namespace WPFApps
         {
             InitializeComponent();
         }
+
+        private void OnRegButtonClick(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim(); 
+            string pass2 = passBox2.Password.Trim(); 
+            string email = textBoxEmail.Text.Trim();
+
+            if (login.Length<5)
+            {
+                //ToolTip свойство которое выводит подску при наведении мышки на обЪект
+                textBoxLogin.ToolTip = "это поле введено не корректно!";
+                textBoxLogin.Background = Brushes.DarkRed;
+            }
+            else if(pass.Length<5)
+            {
+                passBox.ToolTip = "это поле введено не корректно!";
+                passBox.Background = Brushes.DarkRed;
+            }
+            else if (pass!= pass2)
+            {
+                passBox2.ToolTip = "это поле введено не корректно!";
+                passBox2.Background = Brushes.DarkRed;
+            }
+            else if (email.Length<5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "это поле введено не корректно!";
+                textBoxEmail.Background = Brushes.DarkRed;
+            }
+
+            //Если все проверки прошли успешно то очищаем поля от всплывающих подсказок
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+
+                passBox2.ToolTip = "";
+                passBox2.Background = Brushes.Transparent;
+
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Все гуд!");
+            }
+        }
     }
 }
